ac31b8880b5bcea99a78a3c1aa97d90e44490184
naveen khuntey <96486321+naveen-khuntey@users.noreply.github.com>
2023-06-05 20:16:59 +0530

    abstract

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header-body'>
+      <div className='title-header'>
+        <p>abstract</p>
+        <p>|Help Center</p>
+      </div>
+      <div className='header-button'>
+        <button>Submit a Request</button>
+        <button>Sign In</button>
+      </div>
+    </div>
+  )
+}
